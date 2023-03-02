c7875fb16156b01fd70fc71b25ec1b7459f84985
John Hobbs <john@velvetcache.org>
2023-03-02 16:59:43 -0600

    Install Martian Mono

--- software.sh
+++ software.sh
@@ -25,3 +25,12 @@ printf "\033[1;35m=====================\033[0m\n"
 printf "\033[1;35mInstalling fzf config\033[0m\n"
 printf "\033[1;35m=====================\033[0m\n\n"
 "$(brew --prefix)"/opt/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
+
+
+echo
+printf "\033[1;35m=======================\033[0m\n"
+printf "\033[1;35mInstalling Martian Mono\033[0m\n"
+printf "\033[1;35m=======================\033[0m\n\n"
+wget -qO /tmp/martian-mono.zip https://github.com/evilmartians/mono/releases/download/v1.0.0/martian-mono-1.0.0-ttf.zip
+unzip /tmp/martian-mono.zip -d ~/Library/Fonts/ -x '__MACOSX/*' 'README.txt' 'Changelog.md'
+rm /tmp/martian-mono.zip
