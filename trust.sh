94f014e33622f9ad2434e2830eef1f70941feb28
John Hobbs <john@velvetcache.org>
2023-03-22 12:35:11 -0500

    Swap gpg-suite for vanilla install to get newer versions

--- trust.sh
+++ trust.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Fix permissions after symlinking
 chmod 0700 ~/.gnupg
 
+# Write defaults on macOS so pinentry does not cache in keychain
+uname -a | grep Darwin > /dev/null && defaults write org.gpgtools.common "UseKeychain" -bool "false"
+
+# Add the GitHub web flow key
 curl https://github.com/web-flow.gpg | gpg --import
